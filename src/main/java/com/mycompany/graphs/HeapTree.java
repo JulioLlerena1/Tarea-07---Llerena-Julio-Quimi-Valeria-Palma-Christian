4d14cff641965088ab4b41c49066e9d5bae262ea
Christian Palma Carpio <chripalm@espol.edu.ec>
2025-08-22 23:39:45 -0500

    Update HeapTree.java

--- src/main/java/com/mycompany/graphs/HeapTree.java
+++ src/main/java/com/mycompany/graphs/HeapTree.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.heap;
+package com.mycompany.graphs;
 
 import java.util.Comparator;
 
